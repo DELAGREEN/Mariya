5cd1268e65bfafd39367ff2f7ffde04ad31ae5fe
DELAGREEN <60788008+DELAGREEN@users.noreply.github.com>
2022-08-30 20:57:34 +0300

    Посмотрев ютубчик Налотпаил фигни, но пока что, чем богаты тому и рады =_=

--- Mariya/UI.py
+++ Mariya/UI.py
@@ -5,7 +5,7 @@ from tkinter import messagebox
 
 def AddInn():
     value = Enter_Text_Inn.get()
-    if value != '':
+    if value != '':                                                  
         with open ('dataInn.txt', 'a+', encoding = "utf-8") as file:
             file.write(value + '\n')
         Enter_Text_Inn.delete(0, 'end')
@@ -23,19 +23,19 @@ def EnterClick(A):   # A - это заглушка, не понятно мне пока почему, но работает
 
 
 
-window = tk.Tk()
-window.title('Мария')
-window.geometry('400x300+650+300')
-window.resizable(width=False, height=False)
-window['bg'] = 'gray'
+window = tk.Tk()                                # Создает окно
+window.title('Мария')                           # Наименование окна
+window.geometry('400x300+650+300')              # Размеры окна
+window.resizable(width=False, height=False)     # Пока не понял для чего
+window['bg'] = 'gray'                           # Цвет земли окна или как её называют еще фон
 
-btn_add_inn = tk.Button(window, text='Добавить', command=AddInn, font='Arial 15 bold', fg='black', bg='gray')
-btn_add_inn.place(x=10, y = 100)
-window.bind('<Return>', EnterClick)
-lbPath = tk.Label(window, text='Введите INN', font='Arial 15 bold', fg='black', bg='gray')
+btn_add_inn = tk.Button(window, text='Добавить', command=AddInn, font='Arial 15 bold', fg='black', bg='gray')       # Добавляет кнопку - Добавить
+btn_add_inn.place(x=10, y = 100)                                                                                    # Расположение элемента
+window.bind('<Return>', EnterClick)                                                                                 # Добавляет функцию срабатывания кнопки - Добавить - натино через клавишу - Enter
+lbPath = tk.Label(window, text='Введите INN', font='Arial 15 bold', fg='black', bg='gray')                          # Любой текст
 lbPath.place(x=10, y=5)
 
-Enter_Text_Inn = tk.Entry(fg='black', width = 50)
+Enter_Text_Inn = tk.Entry(fg='black', width = 50)                                                                   # Поле ввода                                                                
 Enter_Text_Inn.place(x=10, y=40)
 
-window.mainloop()
+window.mainloop()                                                                                                   # Ну соответственно луп окна, что бы не закрывалось    
