6d79797bad5124c12c5f6b8b370dbce4b8424e9a
DELAGREEN <60788008+DELAGREEN@users.noreply.github.com>
2023-01-27 22:07:37 +0300

    =_= продолжаем

--- Mariya/data.py
+++ Mariya/data.py
@@ -2,17 +2,26 @@
 from asyncore import readwrite
 from fileinput import filename
 import os.path
+from re import I
 from openpyxl.workbook import Workbook as Workbook
 from openpyxl.reader.excel import load_workbook as load_workbook
 import os
+from main import auto_page_pass 
 
 
+path_to_home_dir = os.getcwd()                          #пусть распложения main
+name_to_dir = path_to_home_dir + '\\data'               #путь к папке
+path_to_data = path_to_home_dir + '\\data\\data.xlsx'   #путь к файлу DATA 
 
-home_dir = os.getcwd()
-name_from_data = home_dir + '\\data\\data.xlsx'
-name_to_dir = home_dir + '\\data'
+test_path = name_to_dir + '\\Итого 01.07.22 (2).xlsx'   #тестовый путь
 
-dataList = []
+
+def list_dir():
+    path_to_dir = os.listdir(name_to_dir)
+    #p = str(*path_to_dir)
+    print(f"список список файлов, из {name_to_dir} >> {path_to_dir}")
+list_dir()
+
 
 
 def work_book():
@@ -20,8 +29,7 @@ def work_book():
     Создаем xlsx файл для записи в него
     '''
     wb = Workbook()
-    wb.save(name_from_data)     # сохнаняем с именем лежащей в переменной
-    pass                        # проход в пустоту
+    wb.save(path_to_data)     # сохнаняем с именем лежащей в переменной
 
 def make_dirs():
     '''
@@ -30,90 +38,127 @@ def make_dirs():
     os.makedirs(name_to_dir)
 
 
-#def append_data(cell.value):
-#    return(data_list.append = cell.value)
-
-
-
-   
-
-
-# Данный блок проверяет создан ли файл базы данных
-# Если нет, то просто создает его
-# Таким образом мы пытаемся обойти ощибки со стороны обработки 
-# Программой
-
-if not os.path.exists(name_to_dir):             # Проверяем есть ли ДИРЕКТОРИЯ с таким именем, если нет, создаем
+''' 
+Данный блок проверяет создан ли файл базы данных
+если нет, то просто создает его
+таким образом мы пытаемся обойти ощибки со стороны 
+обработки программой
+'''
+'''
+Созадаем дерикторию
+'''
+if not os.path.exists(name_to_dir):             # Проверяем есть ли ДИРЕКТОРИЯ с таким именем, если нет, создает
     make_dirs()
-
-if not os.path.exists(name_from_data):           # проверяет существование пути, если нет, вызываем функцию создания
+'''
+Создаем базу данных в xlsx формате
+'''
+if not os.path.exists(path_to_data):           # проверяет существование пути, если нет, вызываем функцию создания
     work_book()                             
                                                 # Сюда нужно еще написать вызов функции которая вызывает окно пользователя и говорит ему
                                                 # что база пустая и необходимо ее пополнить INN для прохождения по ней
 
 
 
-#def read_dataFile():
-#    '''
-#    Построчное чтение xlsx
-#
-#    П.С Есть занимательный факт
-#    если строка пустая возвращает None
-#    Можно использовать как фичу, а можно и нет :D
-#    '''
-#    book = load_workbook(name_from_data, read_only=True)
-#    print(book)
-#    sheet = book["Sheet"]
-#    for row in sheet.iter_rows():                          # чтение построчное 
-#        for cell in row:                                   # чтение строк по клеткам
-#            print(cell.value)
-#
-
-
-
 def read_dataFile2():
-    #import Mariya
     '''
     Построчное чтение xlsx
-
-    П.С Есть занимательный факт
-    если строка пустая возвращает None
-    Можно использовать как фичу, а можно и нет :D
     '''
-    book = load_workbook(name_from_data, read_only=True)
+    book = load_workbook(path_to_data, read_only=True)
     sheet = book["Sheet"]
-    for row in sheet.iter_rows():                               # чтение построчное 
-        for data_list in row:                                   # чтение строк по клеткам
+    for row in sheet.iter_rows(min_row=1, max_col=1, max_row=999):
+        '''
+        nim_row минимальное количество строк 
+        max_col максимальное количество столбцов 
+        max_row максильманое колличество строк
+        '''
+        for data_list in row:
+            #print(data_list)
             data_list = data_list.value
-            dataList.append(data_list)
-            print(dataList)
-            #Mariya.auto_page_pass(data_list)                    # Передаем значение в Main для дальнейшего использования
-        return(data_list)
+            print(data_list)
+            #auto_page_pass(data_list)
+#read_dataFile2()
 
-                     
+def read_dataFile22():
+    '''
+    Построчное чтение xlsx
+    '''
+    wb = Workbook()
+    book = load_workbook(test_path, read_only=True)
+    #sheet = book["Sheet1"]
+    #sheet1 = book.active
+    #print(sheet1)
+    #print(book.get_sheet_names()) #проверка списка страниц
+    i = 2
+    sheet = book.active                                                                 #открываем активную страницу
+    print(sheet.max_row)                                                                #по сути работает как len строк
+    print(sheet.max_column)                                                             #по сути работает как len столбцов
+    for row in sheet.iter_rows(min_col = 1,  min_row=i, max_col=9, max_row=i):
+        '''
+        nim_row минимальное количество строк 
+        max_col максимальное количество столбцов 
+        max_row максильманое колличество строк
+        '''
+        for data in row:
+            #print(data_list)
+            out_data = data.value
+            print(out_data)
+            #auto_page_pass(data_list)
+read_dataFile22()
 
 
-#read_dataFile()
-read_dataFile2()
+def append_in_data():
+    wb = Workbook()
+    i = 1
+    y = 1
+    u = 1
+    data = [[i, str(y)],
+           [y, i, u], 
+           [u]]
+
+    for r in data:
+        wb.active.append(r)
+        i,y,u + 1
+        print(data)
+    #wb.active.auto_filter.ref = "A1:C7"
+    #wb.active.add_filter_column(0.)
+    wb.save("data.xlsx")
+    pass
+#append_in_data()
 
 
 
-#
-#def exampe():
-#    """
-#     Создаем xlsx файл для для записи в него
-#
-#    """
-#    book = openpyxl.Workbook()
-#    book.remove(book.active)
-#
-#def json_data_input():
-#
-#    return
-#
-#
-#
+
+
+def odd_in():
+    '''
+    Работает
+    '''
+
+
+    from openpyxl.styles.borders import Border, Side
+    #from openpyxl import Workbook
+    
+    thin_border = Border(left=Side(style='thin'), 
+                         right=Side(style='thin'), 
+                         top=Side(style='thin'), 
+                         bottom=Side(style='thick'))  #жирная полоса
+    
+    wb = Workbook()
+    ws = wb.active
+   
+    # property cell.border should be used instead of cell.style.border
+    ws.cell(row=3, column=2).border = thin_border
+    wb.save('border_test.xlsx')
+    
+odd_in()
+
+
+
+
+
+def modify_data(record_data):
+    
+    pass
 #def data_inject():
-#    inn = ['5260477034']
+#    
 #    return inn
-#
--- Mariya/main.py
+++ Mariya/main.py
@@ -13,40 +13,25 @@ from selenium.webdriver.chrome.options import Options
 #dataList = data.dataList
 data_list = '2130224767'
 homeDir = os.getcwd()
+#inn_data = '1254'
 
-#Данный блок служит для создания профиля Гугл хрома
-#В частности он редактирует директорию в которую браузер загружает файлы
-
-##chromeOptions = webdriver.ChromeOptions()
-
-#!!!!!!!!!!!!!!АЛЛО --- при установки на новую машину или еще чего случиться 
-#необходимо прогу доработать по Path к папке где это все будет собираться
-#######prefs = {"download.default_directory" : "C:\\Users\\igorm\\Downloads\\test"}
-#######
-#######chromeOptions.add_experimental_option("prefs",prefs)
-#######
-########ВНИМАНИЕ!!!!!!!!!!!!!!!!!!!!НЕ ЗАБЫТЬ КЛИЕНТУ ПРИЛОЖИТЬ webdriver
-#######chromedriver = "C:\\chromedriver.exe"
-#######
-#######browser = webdriver.Chrome(executable_path=chromedriver, chrome_options=chromeOptions)
-
-
-def auto_page_pass(data_list):
+def auto_page_pass(inn_data):
     chrome_options = Options()
     '''
     Драйвер для управления браузером.
     '''
     chromedriver = homeDir + "\\sourses\\chromedriver.exe"      
     prefs = {"profile.default_content_settings.popups": 0,
-                "download.default_directory": homeDir + "\data",                  
-                "directory_upgrade": True}
+             "download.default_directory": homeDir + "\data",                  
+             "directory_upgrade": True}                     
+    #homedir - домашняя директория ку 
     chrome_options.add_experimental_option("prefs", prefs)
     browser = webdriver.Chrome(executable_path=chromedriver, chrome_options=chrome_options)
     browser.get('https://websbor.gks.ru/online/info')
     sleep(5)
     #для поиска элемента в примере используется XPath (F12 в браузере, поиск нужного элемента, ПКМ - Copy - XPath)
     # ввести в поле "Телефон и email" 123
-    browser.find_element("xpath",'//*[@id="inn"]').send_keys(data_list)
+    browser.find_element("xpath",'//*[@id="inn"]').send_keys(inn_data)
     sleep(1)
     #нажатие кнопки 1
     browser.find_element("xpath",'//*[@id="body"]/websbor-statistics-codes/websbor-simple-background/div/article/div/div[1]/div/form/div[3]/button/span').click()
@@ -59,14 +44,5 @@ def auto_page_pass(data_list):
     return 0
 
 
-
-
 if __name__ == '__main__':
-
-    #data.read_dataFile2()
-
-
-    #for data_list in dataList:
-    #    #print(data_list)
-    auto_page_pass(data_list)
-    #
+    pass
