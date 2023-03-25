623ba6598631c3d4450751f3ca0dc106f98d898b
DELAGREEN <60788008+DELAGREEN@users.noreply.github.com>
2023-03-25 15:36:44 +0300

    Requests (#1)
    * Рабочая версия на базе request
    * Попытка реализовать acincio

--- Mariya/main.py
+++ Mariya/main.py
@@ -1,28 +1,37 @@
-from time import sleep
+# -*- coding: utf-8 -*-
 import sys
+import asyncio
+import random
+from time import sleep
 from PySide6.QtWidgets import QApplication, QMainWindow
-from ui import Ui_Dialog
+from ui_module import Ui_Dialog
 import time 
+from report_module import read_exel_inn, checking_existence_files
+from requests_module import get_id, get_organization, report_generation
 
 
-start = time.time()
-
-
-def progress_bar(steps: int, max_lenght: int = 50) -> None:
-	
+async def progress_bar(steps: int, max_lenght: int = 50):
 	step_size = 50 / steps
-
 	#step_size = 100 / max_lenght
-
-
 	for i in range(1, steps + 1):
-		print(f'\r{int(i * step_size)}%', end='')
-		p = i * step_size
-		sleep(0.05)
-	print('')
-	return p 
+		print(f'\r{int(i * step_size)}%', end='')	#очень интересная тема
+		counter = int(i * step_size)
+		await asyncio.sleep(0.05)
+	print('')							#очень интересная тема ^^^
 
-#progress_bar(100)
+
+def request():
+	list_inn = read_exel_inn()
+	for item in list_inn:
+		for inn in item:
+			report_generation(get_organization(get_id(inn)), inn)
+
+
+async def main_Function():
+	try:
+		request()
+	except Exception as ex:
+		print(f'Ошибка: {ex}')
 
 
 class App(QMainWindow):
@@ -30,16 +39,21 @@ class App(QMainWindow):
 		super(App, self).__init__()
 		self.ui = Ui_Dialog()
 		self.ui.setupUi(self)
-	
 
-end = time.time()
-total_time = (end - start)/60
-print(f'Время выполнения: {total_time} мин.')
 
 if __name__ == '__main__':
-	
+	start = time.time()
+
+	checking_existence_files()
+
 	app = QApplication(sys.argv)
 	window = App()
 	window.show()
 
-	sys.exit(app.exec())
+
+	end = time.time()
+	total_time = (end - start)/60
+	print(f'Время выполнения: {total_time} мин.')
+
+
+	sys.exit(app.exec())	
---
+++ Mariya/ui_module.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'untitled.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QLabel, QProgressBar,
+    QPushButton, QSizePolicy, QWidget)
+import asyncio
+
+counter = 0
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.setEnabled(True)
+        Dialog.resize(380, 150)
+        Dialog.setMinimumSize(QSize(380, 100))
+        Dialog.setMaximumSize(QSize(380, 150))
+        Dialog.setCursor(QCursor(Qt.ArrowCursor))
+        Dialog.setMouseTracking(True)
+        Dialog.setTabletTracking(False)
+        Dialog.setFocusPolicy(Qt.NoFocus)
+        Dialog.setContextMenuPolicy(Qt.NoContextMenu)
+        Dialog.setAcceptDrops(False)
+        Dialog.setWindowTitle(u"\u041c\u0430\u0440\u0438\u044f(\u0421\u043e\u0437\u0434\u0430\u043d\u0438\u0435 \u043e\u0442\u0447\u0435\u0442\u0430 \u043f\u043e \u0444\u043e\u0440\u043c\u0430\u043c \u0424\u0421\u0413\u0421)")
+        icon = QIcon()
+        iconThemeName = u"applications-system"
+        if QIcon.hasThemeIcon(iconThemeName):
+            icon = QIcon.fromTheme(iconThemeName)
+        else:
+            icon.addFile(u".", QSize(), QIcon.Normal, QIcon.Off)
+
+        Dialog.setWindowIcon(icon)
+        Dialog.setLayoutDirection(Qt.LeftToRight)
+        Dialog.setAutoFillBackground(False)
+        Dialog.setInputMethodHints(Qt.ImhNone)
+        self.pushButton = QPushButton(Dialog)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setEnabled(True)
+        self.pushButton.setGeometry(QRect(131, 50, 121, 51))
+        self.pushButton.clicked.connect(self.button_clicked)   #Кликабельность кнопки
+        self.pushButton.setCheckable(False)
+        self.pushButton.setAutoRepeat(False)
+        self.progressBar = QProgressBar(Dialog)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setGeometry(QRect(0, 120, 381, 23))
+        #self.progressBar.connect(self.progress)             #прогресс бар
+        self.progressBar.setMaximum(100)
+        #self.progressBar.setValue()
+        self.progressBar.setValue(0)
+        #self.progressBar.setValue.connect(self.update)                #значение
+        self.progressBar.setAlignment(Qt.AlignBottom|Qt.AlignHCenter)
+        self.progressBar.setTextVisible(True)
+        self.progressBar.setInvertedAppearance(False)
+        self.progressBar.setTextDirection(QProgressBar.TopToBottom)
+        self.label = QLabel(Dialog)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(20, 10, 371, 16))
+        font = QFont()
+        font.setPointSize(12)
+        self.label.setFont(font)
+
+        self.retranslateUi(Dialog)
+
+        self.pushButton.setDefault(True)
+
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+
+    def button_clicked(self):
+        from main import main_Function
+        from report_module import formater_to_exel
+        asyncio.run(main_Function())
+        formater_to_exel()
+        print('-------ВЫПОЛНЕНО-------')
+
+
+    def retranslateUi(self, Dialog):
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"\u0417\u0430\u043f\u0443\u0441\u043a \u0441\u043a\u0440\u0438\u043f\u0442\u0430", None))
+        self.progressBar.setFormat(QCoreApplication.translate("Dialog", u"%p%", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"\u0412\u044b\u0433\u0440\u0443\u0437\u043a\u0430 \u0438 \u0441\u043e\u0437\u0434\u0430\u043d\u0438\u0435 \u043e\u0442\u0447\u0435\u0442\u0430 \u0441 \u0441\u0430\u0439\u0442\u0430 \u0424\u0421\u0413\u0421 (exel)", None))
+        pass
+    # retranslateUi
