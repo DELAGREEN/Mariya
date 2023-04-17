75a30cfa46e8bcd9e1723441c145096fab7c9c9e
DELAGREEN <60788008+DELAGREEN@users.noreply.github.com>
2023-04-17 23:23:49 +0300

    переписана на классы, подключен asyncio

--- Mariya/report_module.py
+++ Mariya/report_module.py
@@ -3,136 +3,167 @@ from openpyxl.workbook import Workbook as Workbook
 from openpyxl.reader.excel import load_workbook as load_workbook
 from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
 import os
-from config_module import(path_to_data_dir, path_to_data, path_to_final_exel_file, path_to_loads_dir) 
+from config_module import path_to_data_dir, path_to_data, path_to_final_exel_file, path_to_loads_dir 
+import asyncio
 
 
-def make_book(path_to_data):
-    '''
-    Создаем xlsx файл для записи в него
-    '''
-    wb = Workbook()           #Создает exel file
-    wb.save(path_to_data)     # сохнаняем с именем лежащей в переменной
+
+class ReportModule(object):
+    def __init__(self) -> None:
+        self.path_to_data_dir = path_to_data_dir
+        self.path_to_data = path_to_data
+        self.path_to_final_exel_file = path_to_final_exel_file
+        #self.list = list
 
 
-def make_dirs(path_to_data_dir):
-    '''
-    Создаем папки для рабочих 'ДЛЯ УДОБСТВА' процессов
-    '''
-    os.makedirs(path_to_data_dir)
+    def make_book(self, path_to_data):
+        '''
+        Создаем xlsx файл для записи в него
+        '''
+        wb = Workbook()                         #Создает exel file
+        wb.save(path_to_data)                   # сохнаняем с именем лежащей в переменной
 
 
-#def add_style():
-#    '''
-#    Работает
-#    '''
-#    #from openpyxl import Workbook
-#    thin_border = Border(left=Side(style='thin'), 
-#                         right=Side(style='thin'), 
-#                         top=Side(style='thin'), 
-#                         bottom=Side(style='thick'))  #жирная полоса
-#    wb = Workbook()
-#    ws = wb.active
-#    ws.cell(row=3, column=2).border = thin_border
-#    wb.save('border_test.xlsx')
+    def make_dirs(self, path_to_data_dir):
+        path_to_data_dir = self.path_to_data_dir
+        '''
+        Создаем папки для рабочих 'ДЛЯ УДОБСТВА' процессов
+        '''
+        os.makedirs(path_to_data_dir)
+
+# region important
+    #def add_style():
+    #    '''
+    #    Работает
+    #    '''
+    #    #from openpyxl import Workbook
+    #    thin_border = Border(left=Side(style='thin'), 
+    #                         right=Side(style='thin'), 
+    #                         top=Side(style='thin'), 
+    #                         bottom=Side(style='thick'))  #жирная полоса
+    #    wb = Workbook()
+    #    ws = wb.active
+    #    ws.cell(row=3, column=2).border = thin_border
+    #    wb.save('border_test.xlsx')
+#endregion
+
+    def top_matrix_to_file(self, path_to_file) -> None:
+        list = ['ИНН', 'Индекс формы', 'Наименование формы', 'Периодичность формы', 'Срок сдачи формы', 
+                'Отчетный период','Комментарий', 'ОКУД', 'Дата актуализации перечня форм']
+                
+        #wb = Workbook()
+        wb = load_workbook(filename= path_to_file)
+        ws = wb.active
+        ws.append(list)
+        wb.save(path_to_file)
+        wb.close
+        return None
+    #top_matrix_to_file(test)
 
 
-def top_matrix_to_file(path_to_file) -> None:
-    list = ['ИНН', 'Индекс формы', 'Наименование формы', 'Периодичность формы', 'Срок сдачи формы', 
-             'Отчетный период','Комментарий', 'ОКУД', 'Дата актуализации перечня форм']
+    def read_exel_inn(self)->list[int]:
+        list_inn = []
+        book = load_workbook(self.path_to_data, read_only=True)
+        sheet = book.active
+        for row in sheet.iter_rows(min_col = 1, max_col=1, min_row=1, max_row = sheet.max_row):
+            #nim_row минимальное количество строк 
+            #max_col максимальное количество столбцов 
+            #max_row максильманое колличество строк
+            for data in row:
+                out = data.value
+                if type(out) is int and out not in list_inn:
+                    list_inn.append(out)
+                    #print(out)
+        return list_inn
+
+    @property
+    def checking_existence_files(self):
+        ''' 
+        Данный блок проверяет создан ли файл базы данных
+        если нет, то просто создает его
+        таким образом мы пытаемся обойти ошибки со стороны 
+        обработки программой
+        '''
+        '''
+        Созадаем дерикторию
+        '''
+        if not os.path.exists(self.path_to_data_dir):               # Проверяем есть ли ДИРЕКТОРИЯ с таким именем, если нет, создает
+            self.make_dirs(self.path_to_data_dir)
+        '''
+        Создаем базу данных в xlsx формате
+        '''
+        if not os.path.exists(self.path_to_data):                   # проверяет существование пути, если нет, вызываем функцию создания
+            self.make_book(self.path_to_data)                             
+                                                                    # Сюда нужно еще написать вызов функции которая вызывает окно пользователя и говорит ему
+                                                                    # что база пустая и необходимо ее пополнить INN для прохождения по ней
+        if not os.path.exists(self.path_to_final_exel_file):
+            self.make_book(self.path_to_final_exel_file)
+            self.top_matrix_to_file(self.path_to_final_exel_file)
+        print('OK')
+        return None
+
+
+    def writer_a_report_file(self, list) -> None:
+        wb = load_workbook(self.path_to_final_exel_file)
+        ws = wb.active
+        ws.append(list)
+        wb.save(self.path_to_final_exel_file)
+        wb.close
+#        return(print(f'writer_a_report_file >> ok'))
+        return None
+
+    def formater_to_exel(self):
+        
+        wb = load_workbook(self.path_to_final_exel_file)
+        ws = wb.active
+        #print(ws.max_row)
+        '''
+        Делаем фиксированный размер колонок по ширине
+        '''
+        ws.column_dimensions['A'].width = 12    
+        ws.column_dimensions['B'].width = 30    
+        ws.column_dimensions['C'].width = 40    
+        ws.column_dimensions['D'].width = 15    
+        ws.column_dimensions['E'].width = 40    
+        ws.column_dimensions['F'].width = 15     
+        ws.column_dimensions['G'].width = 65    
+        ws.column_dimensions['H'].width = 15    
+        ws.column_dimensions['I'].width = 15    
+        
+        ''' 
+        первую строку пропускаем,
+        узнаем максимальное колличество строк в документе
+        '''
+        i = 1
+        while i <= ws.max_row:                                                                           #max_row - узнаем максимальное колличество строк
+            ws[f'A{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    #Выравнивание текста по центру и перенос текста True
+            ws[f'B{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
+            ws[f'C{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
+            ws[f'D{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
+            ws[f'E{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
+            ws[f'F{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
+            ws[f'G{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
+            ws[f'H{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
+            ws[f'I{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
             
-    #wb = Workbook()
-    wb = load_workbook(path_to_file)
-    ws = wb.active
-    ws.append(list)
-    wb.save(path_to_file)
-    wb.close
-    return None
-#top_matrix_to_file(test)
+            
+            '''
+            делаем выравнивание по высоте,
+            т.к G ячейка самая большая, ориентируюсь на нее
+            беру значение длинны списка и делю на /3
+            '''
+            u = len(ws[f'G{i}'].value)
+            level = int(u/3)
 
+            '''
+            минимально допустимая высота ячейки
+            (сделанна для 1о строчных клеток)
+            '''
+            if level < 60:          
+                level = 60
+            ws.row_dimensions[i].height = level                                                                 #размер строки    
+            i+=1
 
-def read_exel_inn(path_to_data = path_to_data):
-    list_inn = []
-    book = load_workbook(path_to_data, read_only=True)
-    sheet = book.active
-    for row in sheet.iter_rows(min_col = 1, max_col=1, min_row=1, max_row = sheet.max_row):
-        #nim_row минимальное количество строк 
-        #max_col максимальное количество столбцов 
-        #max_row максильманое колличество строк
-        for data in row:
-            out = data.value
-            if type(out) is int and out not in list_inn:
-                list_inn.append(out)
-    yield list_inn
+        wb.save(self.path_to_final_exel_file)
+        wb.close
 
-
-def checking_existence_files():
-    ''' 
-    Данный блок проверяет создан ли файл базы данных
-    если нет, то просто создает его
-    таким образом мы пытаемся обойти ощибки со стороны 
-    обработки программой
-    '''
-    '''
-    Созадаем дерикторию
-    '''
-    if not os.path.exists(path_to_data_dir):             # Проверяем есть ли ДИРЕКТОРИЯ с таким именем, если нет, создает
-        make_dirs(path_to_data_dir)
-    '''
-    Создаем базу данных в xlsx формате
-    '''
-    if not os.path.exists(path_to_data):                # проверяет существование пути, если нет, вызываем функцию создания
-        make_book(path_to_data)                             
-                                                        # Сюда нужно еще написать вызов функции которая вызывает окно пользователя и говорит ему
-                                                        # что база пустая и необходимо ее пополнить INN для прохождения по ней
-    if not os.path.exists(path_to_final_exel_file):
-        make_book(path_to_final_exel_file)
-        top_matrix_to_file(path_to_final_exel_file)
-    return None
-
-
-def writer_a_report_file(list, path_to_file = path_to_final_exel_file):
-    wb = load_workbook(path_to_file)
-    ws = wb.active
-    ws.append(list)
-    wb.save(path_to_file)
-    wb.close
-    return(print(f'writer_a_report_file >> ok'))
-
-
-def formater_to_exel(path_to_file = path_to_final_exel_file):
-  
-    wb = load_workbook(path_to_file)
-    ws = wb.active
-    #print(ws.max_row)
-
-    ws.column_dimensions['A'].width = 12    #размер колонки
-    ws.column_dimensions['B'].width = 30    
-    ws.column_dimensions['C'].width = 40    
-    ws.column_dimensions['D'].width = 15    
-    ws.column_dimensions['E'].width = 40    
-    ws.column_dimensions['F'].width = 15     
-    ws.column_dimensions['G'].width = 60    
-    ws.column_dimensions['H'].width = 15    
-    ws.column_dimensions['I'].width = 15    
-    
-    i = 1
-    while i <= ws.max_row:     #max_row - максимальное колличество скрок
-        ws[f'A{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    #Выравнивание текста по центру и перенос текста True
-        ws[f'B{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
-        ws[f'C{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
-        ws[f'D{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
-        ws[f'E{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
-        ws[f'F{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
-        ws[f'G{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
-        ws[f'H{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)    
-        ws[f'I{i}'].alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
-    
-        u = len(ws[f'G{i}'].value)
-        level = int(u/3)
-        if level < 60:
-            level = 60
-        ws.row_dimensions[i].height = level    #размер строки    
-        i+=1
-
-    wb.save(path_to_file)
-    wb.close
--- Mariya/ui_module.py
+++ Mariya/ui_module.py
@@ -84,9 +84,7 @@ class Ui_Dialog(object):
 
     def button_clicked(self):
         from main import main_Function
-        from report_module import formater_to_exel
         asyncio.run(main_Function())
-        formater_to_exel()
         print('-------ВЫПОЛНЕНО-------')
 
 
