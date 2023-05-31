5ccccc98f3b20b40ed564b6dae6036cf48334d5b
delagreen <delagreen@mail.ru>
2023-05-31 21:38:11 +0300

    singleton and tkinter

---
+++ Mariya/ui/mainwindow.py
@@ -0,0 +1,11 @@
+from tkinter import Tk
+from singleton import Singleton
+
+
+class Window(Tk, Singleton):
+    def init(self):
+        print('calling from init')
+        super().__init__()
+
+    def __init__(self):
+        print('calling from __init__')
