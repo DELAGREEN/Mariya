7b521f6843e1cd3b9964541a8d5a73b94ebb6b5e
DELAGREEN <60788008+DELAGREEN@users.noreply.github.com>
2023-06-10 02:21:54 +0300

    + error loger

--- Mariya/errors_loger.py
+++ Mariya/errors_loger.py
@@ -1,13 +1,14 @@
-import sys
-
+import logging
+from config_module import current_day, current_time
 class ErrorsLoger():
     def __init__(self, error_message) -> None:
-        self.error_message = error_message
-        pass
+        self._error_message = error_message
+        self._current_day = current_day
+        self._current_time = current_time
 
-    def print_error_not_found_organization(self) -> None:
-        print(f'[ERROR/Ошибка] Организация с ИНН: {self.error_message} не найдена.')
-        return None
-    
-    def write_in_txt_log_file(self):
-        pass
+    def print_error_not_found_organization(self):
+        logging.basicConfig(level=logging.INFO, 
+                            filename="py_log.log",
+                            filemode="a+",
+                            format="%(asctime)s %(levelname)s %(message)s")
+        logging.error(f'Организация с ИНН: {self._error_message} не найдена.')
