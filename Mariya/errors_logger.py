0da5d4bb451ca26375c5970765936538b1af2163
DELAGREEN <60788008+DELAGREEN@users.noreply.github.com>
2023-06-10 15:21:34 +0300

    Рефач

--- Mariya/errors_logger.py
+++ Mariya/errors_logger.py
@@ -1,13 +1,13 @@
 import logging
 from config_module import current_day, current_time
 class ErrorsLogger():
-    def __init__(self, error_message) -> None:
-        self._error_message = error_message
+    def __init__(self, message = None) -> None:
+        self._message = message
         self._current_day = current_day
         self._current_time = current_time
 
-    def _log_config(func):
-        def printer(*args, **kwgs):
+    def _log_config(func) -> object:
+        def printer(*args, **kwgs) -> object:
             logging.basicConfig(level=logging.INFO, 
                             filename="py_log.log",
                             filemode="a+",
@@ -16,39 +16,30 @@ class ErrorsLogger():
         return printer
     
     @_log_config
-    def print_error(self):
-        return logging.error(f'Организация с ИНН: {self._error_message} не найдена.')
-        
-        #with open('log.txt', 'a+') as file:
-        #    file.write(f'{self._current_day} {self._current_time} [ERROR/Ошибка] Организация с ИНН: {self._error_message} не найдена.')
+    def print_error(self) -> object:
+        return logging.warning(f'Организация с ИНН: {self._message} не найдена.')
+    @_log_config
+    def print_info(self) -> object:
+        return logging.info(f'{self._message}')
+    @_log_config
+    def print_warning(self) -> object:
+        return logging.warning(f'{self._message}')
+    @_log_config
+    def print_critical(self) -> object:
+        return logging.critical(f'{self._message}')
+    @_log_config
+    def print_debug(self) -> object:
+        return logging.debug(f'{self._message}')
 
-#ErrorsLoger('Message').write_in_txt_log_file()
+'''
+#############
+###Example###
+#############
 
+logging.debug("A DEBUG Message")
+logging.info("An INFO")
+logging.warning("A WARNING")
+logging.error("An ERROR")
+logging.critical("A message of CRITICAL severity")
 
-#logging.debug("A DEBUG Message")
-#logging.info("An INFO")
-#logging.warning("A WARNING")
-#logging.error("An ERROR")
-#logging.critical("A message of CRITICAL severity")
-
-
-
-class MyClass:
-#```
-#В этом примере класс `MyClass` имеет метод `my_method`, который принимает два аргумента `arg1` и `arg2`. Этот метод может быть декорирован декоратором `my_decorator`.
-#Для создания декоратора можно использовать синтаксис `@decorator_name`:
-#```python
-    def my_decorator(func):
-        def wrapper(*args, **kwargs):
-            print("Функция декорирована!")
-            return func()
-        return wrapper
- 
- #Здесь мы определяем декоратор `my_decorator`, который принимает функцию `func` в качестве аргумента. Затем мы создаем функцию-обертку `wrapper`, которая выводит сообщение "Функция декорирована!" и затем вызывает исходную функцию `func`. Наконец, мы возвращаем эту функцию-обертку.
-#Затем мы можем использовать этот декоратор внутри класса `MyClass`:
-#```python
-    @my_decorator # decorator_name = 'my_decorator'
-    def my_method():
-        return 'Ghbdtn'
-
-#MyClass().my_method()
+'''
